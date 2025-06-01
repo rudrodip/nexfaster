3140d0b8fd4c49c02e8505d417f52dc5c7d5795b
rudrodip <official.rudrodipsarker@gmail.com>
2025-06-01 23:23:19 +0600

    shell

---
+++ next.config.ts
@@ -0,0 +1,14 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  rewrites: async () => {
+    return [
+      {
+        source: "/((?!api|_next/static|_next/image|favicon.ico|robots.txt).*)",
+        destination: "/shell",
+      },
+    ];
+  },
+};
+
+export default nextConfig;
